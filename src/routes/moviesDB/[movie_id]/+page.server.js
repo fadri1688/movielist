22f90b5d25c3d3aff754abfa7250e8e2db3ef983
fadri1688 <48827105+fadri1688@users.noreply.github.com>
2024-12-11 11:56:35 +0100

    naplify

--- src/routes/moviesDB/[movie_id]/+page.server.js
+++ src/routes/moviesDB/[movie_id]/+page.server.js
@@ -1,3 +1,4 @@
+import { updated } from "$app/stores";
 import db from "$lib/db.js";
 import { error } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
@@ -17,6 +18,20 @@ export async function load({ params }) {
 }
 
 export const actions = {
+  update: async ({ request }) => {
+    const updateData = await request.formData();
+    let movie = {
+      _id: updateData.get("id"),
+      title: updateData.get("title"),
+      year: updateData.get("year"),
+      length: updateData.get("length")
+      /*
+      actors: updateData.get("actors").split(",")
+      */
+  }
+    console.log('update:', movie);
+    await db.updateMovie(movie);
+  },
   delete: async ({ request }) => {
     const data = await request.formData();
     let id = data.get('movie_id');
--- src/routes/moviesDB/[movie_id]/+page.svelte
+++ src/routes/moviesDB/[movie_id]/+page.svelte
@@ -16,16 +16,62 @@
         />
     </div>
     <div>
-        <p>Jahr: {data.movie.year}</p>
-        <p>Dauer: {movie.length} Minuten</p>
-
-        <p>Cast:</p>
-        <ul>
-            {#each data.movie.actors as actor}
-                <li>{actor}</li>
-            {/each}
-        </ul>
-        <form method="post" action="?/delete">
+        <form class="container mt-4" method="post" action="?/update">
+            <div class="mb-3">
+                <label for="title" class="form-label">Filmtitel:</label>
+                <input
+                    name="title"
+                    value="{data.movie.title}"
+                    type="text"
+                    id="title"
+                    class="form-control"
+                    placeholder="Titel eingeben"
+                    required
+                />
+            </div>
+    
+            <div class="mb-3">
+                <label for="year" class="form-label">Jahr:</label>
+                <input
+                    name="year"
+                    value="{data.movie.year}"
+                    type="number"
+                    id="year"
+                    class="form-control"
+                    placeholder="2025"
+                    required
+                />
+            </div>
+    
+            <div class="mb-3">
+                <label for="length" class="form-label">Dauer (in Minuten):</label>
+                <input
+                    name="length"
+                    value="{data.movie.length}"
+                    type="number"
+                    id="length"
+                    class="form-control"
+                    placeholder="z.B. 120"
+                    required
+                />
+            </div>
+    
+            <div class="mb-3">
+                <label for="actors" class="form-label">Cast (Komma-getrennt):</label
+                >
+                <textarea
+                    name="actors"
+                    value="{data.movie.actors}"
+                    id="actors"
+                    class="form-control"
+                    rows="3"
+                    placeholder="z.B. Actor1, Actor2, Actor3"
+                ></textarea>
+                <input name="id" type="hidden" value={data.movie._id} />
+            </div>
+            <button class="btn btn-primary">update</button>
+        </form>
+        <form class="container mt-4" method="post" action="?/delete">
             {console.log(data.movie._id)}
             <input name="movie_id" type="hidden" value={data.movie._id} />
             <button class="btn btn-danger">Delete</button>
